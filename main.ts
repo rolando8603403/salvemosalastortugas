62155328e78a8370b4f24dd65ec10d66c359d658
rolando8603403 <31369616+rolando8603403@users.noreply.github.com>
2021-05-20 12:12:56 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 basic.forever(function () {
     basic.showNumber(input.temperature())
     if (input.temperature() < 26) {
-        basic.showString("T.baja")
+        music.ringTone(784)
         basic.showIcon(IconNames.Sad)
+        basic.showString("T.baja")
     } else if (input.temperature() > 34) {
+        music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.ForeverInBackground)
         basic.showNumber(input.temperature())
         basic.showString("T.alta")
         basic.showIcon(IconNames.Sad)
     } else if (input.temperature() > 26 && input.temperature() <= 34) {
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.ForeverInBackground)
         basic.showNumber(input.temperature())
         basic.showString("T.normal")
         basic.showIcon(IconNames.Yes)
